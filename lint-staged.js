ca78a91810169e65f50ab3c4ce746204bdf263fd
Gianluca Riggio <luca@maximumawesome.nl>
2023-04-05 19:57:51 +0200

    added PHP-CS-Fixer

--- lint-staged.js
+++ lint-staged.js
@@ -1,4 +1,7 @@
 module.exports = {
-  "*.php": (files) => ["./vendor/bin/phpstan analyse --memory-limit=2G"],
-  "composer.json": () => "composer validate --no-check-publish --strict",
-};
+  '*.php': (files) => [
+    './vendor/bin/phpstan analyse --memory-limit=2G',
+    `./vendor/bin/php-cs-fixer fix --config .php-cs-fixer.php ${files.join(' ')}`,
+  ],
+  'composer.json': () => 'composer validate --no-check-publish --strict',
+}
