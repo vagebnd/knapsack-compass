f6712aba8acc4a345569d4062135521b3f34dd50
Gianluca Riggio <luca@maximumawesome.nl>
2023-04-05 16:17:44 +0200

    removed pint

--- lint-staged.js
+++ lint-staged.js
@@ -1,9 +1,4 @@
-const micromatch = require("micromatch");
-
 module.exports = {
-  "*.php": (files) => [
-    `./vendor/bin/pint --config resources/config/pint.json`,
-    "./vendor/bin/phpstan analyse --memory-limit=2G",
-  ],
+  "*.php": (files) => ["./vendor/bin/phpstan analyse --memory-limit=2G"],
   "composer.json": () => "composer validate --no-check-publish --strict",
 };
