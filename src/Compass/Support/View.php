cb286b0403e46ec8ba107b79cc3180c1c60a4b54
Gianluca Riggio <luca@maximumawesome.nl>
2023-04-06 20:59:29 +0200

    implemented standalone blade compiler

--- src/Compass/Support/View.php
+++ src/Compass/Support/View.php
@@ -2,32 +2,31 @@
 
 namespace Knapsack\Compass\Support;
 
-use Illuminate\Support\Traits\ForwardsCalls;
-use Jenssegers\Blade\Blade;
+use Knapsack\Compass\Contracts\FilesystemContract;
 use Knapsack\Compass\Contracts\ViewContract;
+use Knapsack\Compass\Support\Traits\ForwardsCalls;
+use Knapsack\Compass\Support\View\BladeCompiler;
 
 class View implements ViewContract
 {
     use ForwardsCalls;
 
-    protected Blade $blade;
+    protected $blade;
 
     public function __construct()
     {
-        $this->blade = new Blade(
+        $this->blade = new BladeCompiler(
             vgb_resource_path('views'),
-            vgb_storage_path('framework/views')
+            vgb_storage_path('framework/views'),
+            BladeCompiler::MODE_AUTO,
         );
+
+        $this->blade->setCompiledExtension('.php');
     }
 
     public function render(string $view, array $data = [])
     {
-        return $this->blade->make($view, $data)->render();
-    }
-
-    public function exists(string $view)
-    {
-        return $this->blade->exists($view);
+        return $this->blade->run($view, $data);
     }
 
     public function __call($method, $parameters)
