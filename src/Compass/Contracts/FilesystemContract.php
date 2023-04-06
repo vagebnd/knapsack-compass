372c33d14df373a76c5b871cf8a33b7b1680d1b5
Gianluca Riggio <luca@maximumawesome.nl>
2023-04-06 20:56:42 +0200

    take ownership of contracts

---
+++ src/Compass/Contracts/FilesystemContract.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Knapsack\Compass\Contracts;
+
+interface FilesystemContract
+{
+}
--- src/Compass/Contracts/ViewContract.php
+++ src/Compass/Contracts/ViewContract.php
@@ -5,6 +5,4 @@ namespace Knapsack\Compass\Contracts;
 interface ViewContract
 {
     public function render(string $view, array $data = []);
-
-    public function exists(string $view);
 }
