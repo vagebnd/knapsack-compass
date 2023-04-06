2f2edc0219f0b9aa5f42cfdc937579477114e58c
Gianluca Riggio <luca@maximumawesome.nl>
2023-04-06 20:58:53 +0200

    Ensure that Whoops exists

--- src/Compass/Exceptions/Handler.php
+++ src/Compass/Exceptions/Handler.php
@@ -17,15 +17,19 @@ class Handler implements ExceptionHandler
     public function render(Throwable $e)
     {
         if (wp_get_environment_type() === 'local') {
-            $pageHandler = (new PrettyPageHandler)
-                ->setEditor(Config::get('app.editor', 'vscode'));
+            if (class_exists('\Whoops\Run')) {
+                $pageHandler = (new PrettyPageHandler)
+                    ->setEditor(Config::get('app.editor', 'vscode'));
 
-            $whoops = new \Whoops\Run;
-            $whoops->allowQuit(false);
-            $whoops->writeToOutput(false);
-            $whoops->pushHandler($pageHandler);
+                $whoops = new \Whoops\Run;
+                $whoops->allowQuit(false);
+                $whoops->writeToOutput(false);
+                $whoops->pushHandler($pageHandler);
 
-            echo $whoops->handleException($e);
+                echo $whoops->handleException($e);
+            }
+
+            throw $e;
         } else {
             status_header($e->getCode(), $e->getMessage());
 
