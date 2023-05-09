ba4eed0554c4d878b7ac7bffa6cc7508685ac05a
Gianluca Riggio <luca@maximumawesome.nl>
2023-05-09 15:05:10 +0200

    added typehints

--- src/Compass/Support/Facades/Route.php
+++ src/Compass/Support/Facades/Route.php
@@ -5,6 +5,10 @@ namespace Knapsack\Compass\Support\Facades;
 use Knapsack\Compass\Routing\Router;
 use Knapsack\Compass\Support\Facade;
 
+/**
+ * @method static void template(string $template, string|callable|null $action)
+ * @method static void adminPage(string $endpoint, string|callable|null $action, ?array $config = [])
+ */
 class Route extends Facade
 {
     public static function getFacadeAccessor()
