5da9838afa5bd948456f788e7f306d67b117ae55
Tom Huynen <tom@maximumawesome.nl>
2023-05-26 15:30:11 +0200

    phpdocs

--- src/Compass/Support/Facades/Route.php
+++ src/Compass/Support/Facades/Route.php
@@ -3,11 +3,15 @@
 namespace Knapsack\Compass\Support\Facades;
 
 use Knapsack\Compass\Routing\Router;
+use Knapsack\Compass\Routing\RouteRegistrar;
 use Knapsack\Compass\Support\Facade;
 
 /**
- * @method static void template(string $template, string|callable|null $action)
- * @method static void adminPage(string $endpoint, string|callable|null $action, ?array $config = [])
+ * @method static mixed adminPage(string $endpoint, callable $callback = null, array $config = [])
+ * @method static RouteRegistrar admin()
+ * @method static void post(string $path, array $action)
+ * @method static void get(string $path, array $action)
+ * @method static void delete(string $path, array $action)
  */
 class Route extends Facade
 {
