9a5cd8df53628b3edc697bd839ee7927a81761c2
Gianluca Riggio <luca@maximumawesome.nl>
2023-06-28 13:20:16 +0200

    feat: make views and compiled paths configurable

--- src/Compass/Support/View.php
+++ src/Compass/Support/View.php
@@ -4,6 +4,7 @@ namespace Knapsack\Compass\Support;
 
 use Knapsack\Compass\App;
 use Knapsack\Compass\Contracts\ViewContract;
+use Knapsack\Compass\Support\Facades\Config;
 use Knapsack\Compass\Support\Traits\ForwardsCalls;
 use Knapsack\Compass\Support\View\BladeCompiler;
 
@@ -16,9 +17,7 @@ class View implements ViewContract
     public function __construct(App $app)
     {
         $this->blade = new BladeCompiler(
-            $app->resourcePath(),
-            $app->storagePath('framework/views'),
-            BladeCompiler::MODE_AUTO,
+            ...$this->getCompilerSettings($app),
         );
 
         $this->blade->setCompiledExtension('.php');
@@ -32,5 +31,17 @@ class View implements ViewContract
     public function __call($method, $parameters)
     {
         return $this->forwardCallTo($this->blade, $method, $parameters);
+    }
+
+    private function getCompilerSettings(App $app)
+    {
+        $viewsPath = Config::get('views.paths', [$app->resourcePath('views')]);
+        $compiledPath = Config::get('views.compiled', $app->storagePath('framework/views'));
+
+        return [
+            $viewsPath,
+            $compiledPath,
+            BladeCompiler::MODE_AUTO,
+        ];
     }
 }
